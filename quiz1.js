37d4b75a4bcc7fd419537417d34c20c5d2e09b85
menno weerman <menno.weerman@gmai.com>
2021-01-28 09:39:01 +0100

    QuizLAM

---
+++ quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://www.lamlisse.nl/wp-content/uploads/2018/03/LAM-Lisse-logo-white.svg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "In welk Jaar is museum LAM geopend?",
+				"answers": [{
+						"answer": "2000",
+						"feedback": false
+					},
+					{
+						"answer": "2004",
+						"feedback": false
+					},
+					{
+						"answer": "2009",
+						"feedback": true
+					},
+					{
+						"answer": "2011",
+						"feedback": false
+					},
+					{
+						"answer": "2013",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "LAM – Heeft de collectie een thema?",
+				"answers": [{
+						"answer": "Street art",
+						"feedback": false
+					},
+					{
+						"answer": "Food art",
+						"feedback": true
+					},
+					{
+						"answer": "Colorfull art",
+						"feedback": false
+					},
+					{
+						"answer": "Kitsch art",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "wie is in 2021 de huidige eigenaar van het LAM museum?",
+				"answers": [{
+						"answer": "Renzo Martens",
+						"feedback": false
+					},
+					{
+						"answer": "Yinka Shonibare",
+						"feedback": false
+					},
+					{
+						"answer": "Ron Mueck",
+						"feedback": false
+					},
+					{
+						"answer": "Sietske van Zanten",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
---
+++ quiz2.js
@@ -0,0 +1,65 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "Math Trivia  1",
+        "imageURI": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/x3y3z3k-1568041112.jpg?crop=0.987xw:0.790xh;0.00160xw,0.128xh&resize=980:*"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat is 3 x 17? ",
+				"answers": [{
+						"answer": "17",
+						"feedback": false
+					},
+					{
+						"answer": "3",
+						"feedback": false
+					},
+					{
+						"answer": "20",
+						"feedback": false
+					},
+					{
+						"answer": "51",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "69 - 41 =  ?",
+				"answers": [{
+						"answer": "16",
+						"feedback": false
+					},
+					{
+						"answer": "26",
+						"feedback": false
+					},
+					{
+						"answer": "28",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Wat is 19 x 10",
+				"answers": [{
+						"answer": "189",
+						"feedback": false
+					},
+					{
+						"answer": "191",
+						"feedback": false
+					},
+					{
+						"answer": "190",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
